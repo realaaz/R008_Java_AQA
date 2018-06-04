663a07c5cff5ce4970614b11ed43343ef002143f
ivanzakoretskyi <ivnzak@gmail.com>
2018-06-05 02:04:37 +0300

    try to use selenide

---
+++ src/test/java/P004_Selenide/C004_Selenide.java
@@ -0,0 +1,37 @@
+package P004_Selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class C004_Selenide {
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/ivanzakoretskyi/Drivers/chromedriver");
+        Configuration.browser = "chrome";
+        open("https://mail.ukr.net/desktop/login");
+    }
+
+    @Test
+    public void userLogin() {
+        $("#login").setValue("autotestorgua");
+        $("#password").setValue("testpass");
+        $(By.xpath("//button[text()='Войти']")).click();
+        $(".login-button__user").shouldHave(visible, text("autotestorgua@ukr.net"));
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        $(".login-button__menu-icon").click();
+        $("#login__logout").shouldBe(visible).click();
+    }
+
+}
