8f7491b6e28d212fbf75063312a751c944b7faf4
ivanzakoretskyi <ivnzak@gmail.com>
2018-06-06 00:04:59 +0300

    appium neww test - swipe

---
+++ src/test/java/P005_Apium/C005_AppiumTest.java
@@ -0,0 +1,85 @@
+package P005_Apium;
+
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.touch.offset.PointOption.point;
+
+public class C005_AppiumTest {
+
+
+    protected static AndroidDriver<MobileElement> driver;
+
+    public static final String EMAIL = "echornobai@intersog.com";
+    public static final String CORRECT_PWD = "testpass";
+
+
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Google");
+
+        File app = new File("bin/TED_v3.1.20_apkpure.com.apk");
+
+        caps.setCapability(MobileCapabilityType.APP, app.getAbsolutePath()); // ??
+        caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.ted.android");
+        caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.ted.android.view.home.HomeActivity");
+
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+
+    @Test (description = "successLoginTest")
+    public void successLoginTest() {
+        MobileElement profile = driver.findElementByAccessibilityId("My talks");
+        profile.click();
+        MobileElement loginSection = driver.findElementById("myTalksLoggedOutActionLogin");
+        loginSection.click();
+        MobileElement emailInput = driver.findElementById("user_email");
+        emailInput.sendKeys(EMAIL);
+        MobileElement pwdInput = driver.findElementById("user_password");
+        pwdInput.sendKeys(CORRECT_PWD);
+        MobileElement logInBtn = driver.findElementByAccessibilityId("Log in");
+        logInBtn.click();
+        MobileElement OKButton = driver.findElementById("button2");
+        OKButton.click();
+        MobileElement loggedInTitle = driver.findElementById("myTalksLoggedInTitle");
+
+        Assert.assertEquals(loggedInTitle.getText(), "Elena Chornobai ");
+    }
+
+    @Test (description = "swipeToSurprisePLSection")
+    public void swipeToSurprisePLSection() {
+        new TouchAction(driver).longPress(point(109, 1370)).moveTo(point(882, 1370)).release().perform();
+        MobileElement sectionName = driver.findElementById("sectionText");
+        Assert.assertEquals(sectionName.getText(), "Surprise me");
+    }
+
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+}
